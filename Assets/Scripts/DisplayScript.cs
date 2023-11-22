5d38a8bbef455c57d6331b8a380015cdc41e7c2f
Trapland <89914789+Trapland@users.noreply.github.com>
2023-11-22 09:07:50 +0100

    flappy upd

---
+++ Assets/Scripts/DisplayScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayScript : MonoBehaviour
+{
+    private TMPro.TextMeshProUGUI clock;
+    private float gameTime;
+    
+    void Start()
+    {
+        // Знайти компонент TextMeshProUGUI у іншого GameObject "ClockTMP"
+        clock = GameObject.Find("ClockTMP").GetComponent<TMPro.TextMeshProUGUI>();
+        gameTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameTime += Time.deltaTime;
+    }
+    private void LateUpdate()
+    {
+        int time = (int)gameTime;
+        int hour = time / 3600;
+        int minute = (time % 3600) / 60;
+        int second = time % 60;
+        int decisecond = (int)((gameTime - time) * 10);
+        clock.text = $"{hour:00}:{minute:00}:{second:00}.{decisecond:0}";
+    }
+}
